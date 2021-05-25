d0275cf90db8a2f80f6fa6ba27d3d176fc026fbf
Fazle Bari <fazlebarisn@gmail.com>
2021-05-25 23:34:46 +0600

    add menu and submenu in wordpress dashbord

--- fbs-dev.php
+++ fbs-dev.php
@@ -29,3 +29,26 @@ function fbs_dev_enqueue_script(){
     // include js file
     wp_enqueue_script( 'fbs_dev_script', plugin_dir_url(__FILE__) . "assets/js/custom.js", array(), '1.0.0', true );
 }
+
+
+// add menu in wordpress dashbord
+add_action( 'admin_menu', 'fbs_dev_munu' );
+
+function fbs_dev_munu(){
+    add_menu_page( 'Fbs Dev', 'Fbs Dev Options', 'manage_options', 'fbs_dev_options', 'fbs_dev_menu_func', '', null );
+
+    add_submenu_page( 'fbs_dev_options', 'Fbs Settings', 'Fbs Settings', 'manage_options', 'fbs_settings', 'fbs_settings_func', null );
+    add_submenu_page( 'fbs_dev_options', 'Fbs Layout', 'Fbs Layout', 'manage_options', 'fbs_layout', 'fbs_layout_func', null );
+}
+
+function fbs_dev_menu_func(){
+    echo "<h1>Fbs Options</h1>";
+}
+
+function fbs_settings_func(){
+    echo "<h1>Fbs Settings</h1>";
+}
+
+function fbs_layout_func(){
+    echo "<h1> Fbs Layout</h1>";
+}
