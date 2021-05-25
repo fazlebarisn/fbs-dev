0946e9f61732575a2487fca14689f27e12429cd1
Fazle Bari <fazlebarisn@gmail.com>
2021-05-25 18:21:14 +0600

    Start building

---
+++ fbs-dev.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package FbsDevPlugin
+*/
+
+/*
+Plugin Name: Fbs Dev Plugin
+Plugin URI: https://www.chitabd.com/
+Description: My New Plugin
+Version: 1.0.0
+Author: Fazle Bari
+Author URI: https://www.chitabd.com/
+Licence: GPL Or leater
+Text Domain: fbs-dev
+*/
+
+/*
+This is a free plugin
+*/
