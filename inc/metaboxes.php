d097ddd4834a794e903986ac65da53f012f47d42
Fazle Bari <fazlebarisn@gmail.com>
2021-05-27 11:56:38 +0600

    add setect metabox

--- inc/metaboxes.php
+++ inc/metaboxes.php
@@ -7,10 +7,20 @@ add_action( 'admin_init' , function(){
 function fbs_dev_custom_metabox( $post ){
 
     $_mymetabox = get_post_meta( $post->ID, '_mymetabox' , true ) ? get_post_meta( $post->ID, '_mymetabox' , true ) : '';
+    $_fbs_dev_city = get_post_meta( $post->ID, '_fbs_dev_city' , true ) ? get_post_meta( $post->ID, '_fbs_dev_city' , true ) : '';
 
     //var_dump($_mymetabox);
     ?>
-        <input type="text" name="_mymetabox" id="" class="" value="<?php echo $_mymetabox ?>">
+        <label for="fbs_dev_metabox_name">Name :
+            <input type="text" name="_mymetabox" id="" class="" value="<?php echo $_mymetabox ?>">
+        </label>
+        <label for="fbs_dev_metabox_city"> City :
+            <select name="_fbs_dev_city" id="">
+                <option value="Dhaka" <?php echo $_fbs_dev_city== "Dhaka" ? 'selected' : '' ?>>Dhaka</option>
+                <option value="Rajshahi" <?php echo $_fbs_dev_city== "Rajshahi" ? 'selected' : '' ?>>Rajshahi</option>
+                <option value="Naogaon" <?php echo $_fbs_dev_city== "Naogaon" ? 'selected' : '' ?>>Naogaon</option>
+            </select>
+        </label>
     <?php
 }
 
@@ -18,7 +28,8 @@ add_action( 'save_post', 'fbs_dev_save_post' );
 
 function fbs_dev_save_post( $post_id ){
 
-    if( array_key_exists( '_mymetabox' , $_POST )){
+    if( array_key_exists( '_mymetabox' , $_POST ) && array_key_exists( '_fbs_dev_city' , $_POST ) ){
         update_post_meta( $post_id , '_mymetabox' , $_POST['_mymetabox'] );
+        update_post_meta( $post_id , '_fbs_dev_city' , $_POST['_fbs_dev_city'] );
     }
 }
