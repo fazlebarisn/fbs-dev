9fa6571860fe47c3f5b73b1e94e4f1dc53785b6e
Fazle Bari <fazlebarisn@gmail.com>
2021-05-28 16:04:18 +0600

    pagination added on news shortcode

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -8,27 +8,41 @@ function fbs_dev_init(){
     add_shortcode( 'news', 'news_shortcode' );
 }
 
-// define test shortcode function
+// define news shortcode function
 function news_shortcode( $atts , $content='' ){
     
     $atts = shortcode_atts( array(
-        'message' => 'default message',
+        'posts_per_page' => -1,
     ), $atts, 'news' );
 
+    $paged = ( get_query_var('paged') ) ?  get_query_var('paged') :1;
     $args = array(
         'post_type' => 'news',
         'post_status' => 'publish',
-        'posts_per_page' => -1,
+        'posts_per_page' => $atts['posts_per_page'],
+        'paged' => $paged,
     );
 
     $query = new WP_Query( $args );
 
     if( $query->have_posts()):
+
         while( $query->have_posts() ): 
             $query->the_post();
-            $content.= "<h2><a href='". get_the_permalink() ."'>" . get_the_title() . "</a></h2>";
-            $content.= "<p>" . get_the_content() . "</p>";
+            $content.= "<article id='post-news-" . get_the_ID() ."'>";
+            $content.= "<h2><a class='news_title' href='". get_the_permalink() ."'>" . get_the_title() . "</a></h2>";
+            $content.= "<p class='news_content'>" . get_the_content() . "</p>";
+            $content.= "</article>";
         endwhile;
+
+        if( $atts['posts_per_page'] > 0 ) :
+            $content.= "<nav class='next_previous'>";
+            $content.= get_next_posts_link( 'Older News' , $query->max_num_pages );
+            $content.= get_previous_posts_link( 'Latest News');
+            $content.= "</nav>";
+        endif;
+
+        wp_reset_query();
     endif;
 
     return $content;
